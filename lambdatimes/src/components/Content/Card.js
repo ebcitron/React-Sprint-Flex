2c547c7a74032cfe9b7eab1e6bccba16df9d4f94
Eli Citron <ebcitron@gmail.com>
2019-03-01 12:42:54 -0600

    functional

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{props.headline}</div>
+      <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.img} alt="Image"/>
+          <img src={props.card.img} alt="Image"/>
         </div>
         <span>By {props.card.author}</span>
       </div>
